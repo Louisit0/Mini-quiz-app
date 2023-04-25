64cccb6cb326aed8c46aa94424f2f4a35b9b24b8
Luis <lrangarita7@gmail.com>
2023-04-24 21:02:57 -0300

    Fix submitAnswer. Changes on responsive design.

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -5,20 +5,18 @@ const Questions = ({ quizQuestions }) => {
   const [answers, setAnswers] = useState(0);
 
   const submitAnswer = (index) => {
-    setNumQuestion(numQuestion + 1);
-    console.log("numQuestion" + numQuestion);
     if (quizQuestions[numQuestion].options[index].isCorrect === true) {
       setAnswers(answers + 1);
-      console.log(answers);
     }
+    setNumQuestion(numQuestion + 1);
   };
 
   return (
-    <div className="w-4/5 h-3/4 md:w-1/2 md:h-1/2 bg-zinc-800 rounded-xl m-auto flex flex-col md:flex-row">
+    <div className="w-4/5 h-full my-28 mx-auto md:m-auto md:w-1/2 md:h-1/2 bg-zinc-800 rounded-xl flex flex-col md:flex-row">
       {/* Desde aca va el conditional rendering */}
       {numQuestion < quizQuestions.length ? (
         <>
-          <div className="md:w-1/2 mx-4 text-center mt-6">
+          <div className="w-full md:w-1/2 text-center mt-6">
             <h2 className="text-white text-4xl md:text-5xl">
               Question {numQuestion + 1}/
               <span className="text-2xl md:text-3xl">4</span>
@@ -29,7 +27,7 @@ const Questions = ({ quizQuestions }) => {
             <img
               src={quizQuestions[numQuestion].img}
               alt="question"
-              className="mx-auto mt-4 w-1/4 md:w-2/3 md:h-1/2"
+              className="mx-auto mt-4 w-3/4 md:w-2/3 md:h-1/2 rounded-md"
             />
           </div>
           <div className="w-full md:w-1/2 my-auto">
@@ -37,7 +35,7 @@ const Questions = ({ quizQuestions }) => {
               {quizQuestions[numQuestion].options.map((option, index) => (
                 <li className="text-white text-xl" key={index}>
                   <button
-                    className="py-2 w-3/4 border-2 rounded-xl hover:bg-gray-600 focus:animate-pulse focus:bg-blue-500"
+                    className="py-2 w-3/4 border-2 rounded-md hover:bg-gray-600 focus:animate-pulse focus:bg-blue-500"
                     onClick={() => submitAnswer(index)}
                   >
                     {option.answerText}
@@ -48,13 +46,17 @@ const Questions = ({ quizQuestions }) => {
           </div>
         </>
       ) : (
-        <div className="flex flex-col justify-center items-center w-full">
-          <h2 className="text-white text-4xl md:text-5xl">
-            Congrats! you finished the game 🥳
-          </h2>
-          <p className="text-3xl mt-4 text-blue-400 font-bold">
-            Respondiste bien {answers} de {numQuestion} preguntas!
-          </p>
+        <div className="flex items-center justify-center flex-grow flex-col">
+          <div>
+            <h2 className="text-white text-center text-3xl md:text-5xl">
+              Congrats! you finished the game 🥳
+            </h2>
+          </div>
+          <div>
+            <p className="text-2xl mt-4 text-center text-blue-400 font-bold">
+              Respondiste bien {answers} de {quizQuestions.length} preguntas!
+            </p>
+          </div>
         </div>
       )}
 
